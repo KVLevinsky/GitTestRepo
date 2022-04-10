00c2ad7d0cc02388a594d686589a023d0c79e07c
Konstantin Levinsky <kvlevinsky@outlook.com>
2022-04-10 13:21:29 +0200

    Code editor commit

--- Test.py
+++ Test.py
@@ -1,5 +1,12 @@
+def Test(values):
+    for value in values:
+        print(value)
+    print(f"{len(values)} values have been tested")
+
+
 def main():
     print("In main function")
+    Test([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 
 if __name__ == '__main__':
